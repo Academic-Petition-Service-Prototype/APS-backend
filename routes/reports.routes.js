0735d75a2e199402d9584614d13c78cbeeeb5db0
Simon Hope <mee_zozaa@hotmail.com>
2021-12-03 15:44:30 +0700

    Add report routes, add dockerfile

--- routes/reports.routes.js
+++ routes/reports.routes.js
@@ -4,12 +4,46 @@ const router = require('express').Router();
 // Database
 const db = require('../lib/connectdatabase');
 
+// Get all report
+router.get('/reports',(req, res) => {
+    db.query('SELECT * FROM reports',(err, rows, fields) => {
+        if(!err){
+            res.send(rows);
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+// Get report by id
+router.get('/reports/:id',(req, res) => {
+    let id = req.params.id;
+    db.query('SELECT * FROM reports WHERE id = ?',[req.params.id],(err, rows, fields) => {
+        if(rows.length <= 0){
+            res.send('Report not found with id = ' + id)
+        } else {
+            res.send(rows[0]);
+        }
+    })
+})
+
+// Change state report
+router.put('/reports',(req, res) => {
+    let id = req.body.id;
+    db.query(`UPDATE reports SET report_state = 'read' WHERE reports.id = ${id};`,(err, rows, fields) => {
+        if(!err){
+            res.send('Change state success');
+        } else {
+            res.send(err);
+        }
+    })
+})
+
 // Insert reports
 router.post('/reports',(req, res) => {
     let report_title = req.body.report_title;
     let report_detail = req.body.report_detail;
     let errors = false;
-
     if(report_title.length === 0 || report_detail.length === 0){
         errors = true;
         res.send('Please fill your information');
