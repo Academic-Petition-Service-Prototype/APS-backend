899e5b17a8cb173e8346b5f41d061cce52719a0f
Simon Hope <mee_zozaa@hotmail.com>
2022-02-17 22:31:51 +0700

    Add get all form

--- routes/forms.routes.js
+++ routes/forms.routes.js
@@ -4,12 +4,56 @@ const router = require('express').Router();
 // Database
 const db = require('../lib/connectdatabase.js');
 
+// Get all forms
+router.get('/forms',(req, res) => {
+    let user_id = req.body.user_id;
+    let role = req.body.role;
+    let agency = req.body.agency;
+    if (role === 'admin') {
+        db.query(`SELECT form_id,form_name,form_specific,created_date,approval_name,form_status,users_id FROM forms 
+        INNER JOIN users on forms.users_id = user_id `,(err, rows, fields) => {
+            console.log(rows);
+            if(!err){
+                res.send(rows);
+            } else {
+                console.log(err)
+            }
+        })
+    } else if(role === 'chief'){
+        db.query(`SELECT form_id,form_name,form_specific,created_date,approval_name,form_status,users_id FROM forms 
+        INNER JOIN users on forms.users_id = user_id 
+        INNER JOIN agency on agency_id = users.agencies_id 
+        AND agency.agency_name = ?`,agency,(err, rows, fields) => {
+            console.log(rows);
+            if(!err){
+                res.send(rows);
+            } else {
+                console.log(err)
+            }
+        })
+    } else if(role === 'officer'){
+        db.query(`SELECT form_id,form_name,form_specific,created_date,approval_name,form_status,users_id FROM forms 
+        WHERE users_id = ?`,user_id,(err, rows, fields) => {
+            console.log(rows);
+            if(!err){
+                res.send(rows);
+            } else {
+                console.log(err)
+            }
+        })
+    }else {
+        console.log(err)
+        res.send(err);
+    }
+    
+})
+
 // Insert forms
 router.post('/forms',(req, res) => {
     let form_name = req.body.form_name;
     let form_specific = req.body.form_specific;
     form_specific = JSON.stringify(form_specific);
-    let created_by = req.body.f_name + ' ' + req.body.l_name;
+    let created_by = req.body.users_id;
     let approval_name = req.body.approval_name;
     approval_name = JSON.stringify(approval_name);
     let form_status = 'active';
@@ -28,8 +72,8 @@ router.post('/forms',(req, res) => {
                     //error
                     res.send('ชื่อคำร้องนี้มีอยู่ในระบบแล้ว');
                 } else { //form name not in use
-                    db.query(`INSERT INTO forms (form_name, form_specific, created_date, created_by, approval_name,form_status ) 
-                            VALUES ('${form_name}', '${form_specific}', now(), '${created_by}', '${approval_name}', '${form_status}');`,(err, result) => {
+                    db.query(`INSERT INTO forms (form_name, form_specific, created_date, approval_name, form_status, users_id) 
+                            VALUES ('${form_name}', '${form_specific}', now(), '${approval_name}', '${form_status}', '${created_by}');`,(err, result) => {
                         if(!err){
                             res.send('เพิ่มคำร้องใหม่สำเร็จ');
                         } else {
